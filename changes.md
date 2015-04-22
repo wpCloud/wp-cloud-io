55c24a2cd45d0e13ee38e1e9fd91cff48f3e1c42
Andy Potanin <andy.potanin@usabilitydynamics.com>
2015-04-22 18:06:42 -0400

    Added wp-cloud-ssl to improve secure requests by fixing protocol in body output.

--- changes.md
+++ changes.md
@@ -1,3 +1,4 @@
 ### 0.5.0
+* Added wp-cloud-ssl to improve secure requests by fixing protocol in body output.  
 * Making wp-cloud-navbar and wp-cloud be auto-enabled as a site-wide plugin via MU dropin.
 * Initial setup.
--- wp-cloud-ssl.php
+++ wp-cloud-ssl.php
@@ -18,12 +18,32 @@ namespace wpCloud\SSL {
     include_once( __DIR__ . '/vendor/libraries/autoload.php' );
   }
 
-  // add_filter( 'option_active_plugins', 'wpCloud\Policy\Filter::active_plugins' );
-  // add_filter( 'site_option_active_sitewide_plugins', 'wpCloud\Policy\Filter::sitewide_plugins' );
-
-
+  add_action( 'option_active_plugins', 'wpCloud\SSL\Action::init' );
 
   class Action {
+
+    static public function init() {
+
+      if( is_ssl() ) {
+
+        ob_start( function( $buffer ) {
+
+          // Known CDNs.
+          $buffer = str_replace( 'http://ajax.googleapis.com', 'https://ajax.googleapis.com', $buffer );
+
+          // Internal/General.
+          $buffer = str_replace( 'href="http://', 'href="https://', $buffer );
+          $buffer = str_replace( 'href=\'http://', 'href=\'https://', $buffer );
+          $buffer = str_replace( 'src=\'http://', 'src=\'https://', $buffer );
+          $buffer = str_replace( 'src="http://', 'src="https://', $buffer );
+
+          return $buffer;
+
+        });
+
+      }
+
+    }
 
   }
 
